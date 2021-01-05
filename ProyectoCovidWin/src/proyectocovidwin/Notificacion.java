b2bbefccc9d2cdeb98fb92f11f77a33c93c23531
DavidZarate800 <66102574+DavidZarate800@users.noreply.github.com>
2021-01-04 22:52:05 -0600

    Versión final

--- ProyectoCovidWin/src/proyectocovidwin/Notificacion.java
+++ ProyectoCovidWin/src/proyectocovidwin/Notificacion.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectocovidwin;
 
-/**
- *
- * @author David
- */
+import javax.swing.JOptionPane;
+
 public class Notificacion {
+    
+    public Notificacion(String title, String msg) {
+        send(title, msg);
+    }
+    
+    public void send(String title, String msg) {
+        JOptionPane.showMessageDialog(null, msg, title, 0);
+    }
     
 }
--- ProyectoCovidWin/src/proyectocovidwin/ProyectoCovidWin.java
+++ ProyectoCovidWin/src/proyectocovidwin/ProyectoCovidWin.java
@@ -15,7 +15,8 @@ public class ProyectoCovidWin {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+          MenuPrincipal obj = new MenuPrincipal();
+        new Opciones();
     }
     
 }
